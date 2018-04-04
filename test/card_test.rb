4e57fdf3be4263557f9f3e84adba7a77dfe72203
Richard Rosa-Serrano <richardrosaserrano@gmail.com>
2018-04-04 16:52:48 -0600

    Iteration 1

---
+++ test/card_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require 'pry'
+class CardTest < Minitest::Test
+  def test_that_card_has_a_question
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+
+    expected = "What is the capital of Alaska?"
+    assert_equal expected, card.question
+    expected = "Juneau"
+    # binding.pry
+    assert_equal expected, card.answer
+  end
+end
