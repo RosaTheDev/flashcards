8fd10f8154ea61a24ecc55f1fe681c9942fd3106
Richard Rosa-Serrano <richardrosaserrano@gmail.com>
2018-04-04 21:24:11 -0600

    Iteration 4... but the beginning

---
+++ test/round_test.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/guess'
+require './lib/deck'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+  def test_that_it_can_make_a_deck_of_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    assert_equal deck, round.deck
+  end
+
+  def test_that_it_has_an_empty_array_for_guesses
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    assert_equal [], round.guesses
+  end
+
+  def test_the_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+  end
+end
